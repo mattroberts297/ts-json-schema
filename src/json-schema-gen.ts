cf29e80328de0f7cdbf04940b33e1ad5d6386267
Matt Roberts <mattroberts297@users.noreply.github.com>
2018-01-30 20:00:00 +0000

    Get ready for publish

--- src/json-schema-gen.ts
+++ src/json-schema-gen.ts
@@ -1,14 +1,15 @@
-import {parse} from "./json-schema-util";
-import {convert} from "./mustache-data-view-util";
-import {MustacheDataView} from "./mustache-data-view";
-import {readFile, writeFile} from "./fs";
 import * as Mustache from "mustache";
+
+import {readFile, writeFile} from "./fs";
+import {parse} from "./json-schema-util";
+import {MustacheDataView} from "./mustache-data-view";
+import {convert} from "./mustache-data-view-util";
 
 export function generate(schemaPath: string, outputPath: string): Promise<void> {
   function render(view: MustacheDataView): Promise<void> {
-    return readFile("./etc/template.mustache").then(template => {
+    return readFile("./etc/template.mustache").then((template) => {
       return writeFile(outputPath, Mustache.render(template, view));
-    });    
+    });
   }
   return readFile(schemaPath).then(parse).then(convert).then(render);
 }
